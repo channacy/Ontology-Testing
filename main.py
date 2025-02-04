ca1bcdda8e5f0fb17d4b7760a0e6149b653b9a36
Channacy <68759286+channacy@users.noreply.github.com>
2025-02-04 13:15:28 -0500

    added is_a testing

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import csv
 omim_ontology_filepath = "data/omim_susc_import.owl"
 do_ontology_filepath = "data/doid.owl"
 subclasses = {}
-csv_file = "subclasses.csv"
+is_a_classes = {}
+subclasses_csv_file = "subclasses.csv"
+is_a_csv_file = "is_a.csv"
 
 if __name__=="__main__":
     subclasses_dict = {}
@@ -22,8 +24,26 @@ if __name__=="__main__":
                 # print(f"- {subclass.name}")
     #print(subclasses_dict)
 
-    with open(csv_file, 'w', newline='') as file:
+    for cls in omin_onto.classes():
+        classname_parts = str(cls).split(".")
+        classname = classname_parts[1]
+        for superclass in cls.is_a:
+            superclass_name_parts = str(superclass).split(".")
+            superclass_name = superclass_name_parts[1]
+            if superclass_name in is_a_classes:
+                is_a_classes[superclass_name].append(classname)
+            else:
+                is_a_classes[superclass_name] = [classname]
+
+    print(is_a_classes)
+
+    with open(subclasses_csv_file, 'w', newline='') as file:
         writer = csv.writer(file)
         for key, subclasses in subclasses_dict.items():
+            writer.writerow([key, subclasses])
+
+    with open(is_a_csv_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for key, subclasses in is_a_classes.items():
             writer.writerow([key, subclasses])
         
